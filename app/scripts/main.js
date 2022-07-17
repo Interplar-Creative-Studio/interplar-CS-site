8bb6422e03eb0e0e7217eaff08f85616ffd19628
Kerimov Farid <kerimov.f@platrum.ru>
2022-07-17 20:17:24 +0300

    dev

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,11 @@
-const preHeader = document.querySelector('.pre-header')
-const preHeaderText = document.querySelector('.pre-header__content-text');
+/*const preHeader = document.querySelector('.pre-header')
+const preHeaderText = document.querySelector('.pre-header__content-text');*/
+const menuBurger = document.querySelector('.menu__button-mobile');
+const menuButton = document.querySelector('.menu__button');
+const menu = document.querySelector('.menu')
+const menuLink = document.querySelectorAll('.menu-link')
 
-setTimeout(function() {
+/*setTimeout(function() {
     preHeaderText.classList.add('pre-header__content-text-active');
 }, 2000);
 setTimeout(function() {
@@ -9,4 +13,27 @@ setTimeout(function() {
 }, 5000);
 setTimeout(function() {
     preHeader.style.display = "none";
-}, 7000);
+}, 7000);*/
+
+menuBurger.addEventListener('click',() =>{
+    menu.classList.toggle('menu-active');
+});
+menuButton.addEventListener('click',() =>{
+    menu.classList.toggle('menu-active');
+});
+document.addEventListener( 'click', (e) => {
+    let target = e.target;
+    let itsMenu = target === menu || menu.contains(target)
+    let itsBurger = target === menuBurger;
+    let itsButton = target === menuBurger;
+    let menuIsActive = menu.classList.contains('menu-active');
+
+    if (!itsMenu && !itsBurger && menuIsActive) {
+        menu.classList.toggle('menu-active');
+    }
+});
+menuLink.forEach(link =>{
+    link.addEventListener('click',() =>{
+        menu.classList.remove('menu-active');
+    })
+})
