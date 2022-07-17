3aedcdfff6cd8e7bc058c86e48236a968d03a374
Kerimov Farid <kerimov.f@platrum.ru>
2022-07-17 17:09:14 +0300

    dev

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,12 @@
-
-
+const preHeader = document.querySelector('.pre-header')
 const preHeaderText = document.querySelector('.pre-header__content-text');
 
 setTimeout(function() {
     preHeaderText.classList.add('pre-header__content-text-active');
-}, 2000)
+}, 2000);
+setTimeout(function() {
+    preHeader.classList.add('pre-header-hide');
+}, 5000);
+setTimeout(function() {
+    preHeader.style.display = "none";
+}, 7000);
