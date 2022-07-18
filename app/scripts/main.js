b30b9a4ad34dd1b428b20addf92500c188475993
Kerimov Farid <kerimov.f@platrum.ru>
2022-07-19 02:54:37 +0300

    dev

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,7 +6,6 @@ const menuBurger = document.querySelector('.menu__button-mobile');
 const menuButton = document.querySelector('.menu__button');
 const menu = document.querySelector('.menu')
 const menuLink = document.querySelectorAll('.menu-link')
-const pageBody = document.querySelector(':root')
 
 
 setTimeout(function() {
@@ -14,7 +13,6 @@ setTimeout(function() {
 }, 2000);
 setTimeout(function() {
     preHeader.classList.add('pre-header-hide');
-    pageBody.style.overflowY = "scroll";
 }, 4000);
 setTimeout(function() {
     preHeader.style.display = "none";
@@ -45,6 +43,8 @@ menuLink.forEach(link =>{
 new Splide('.splide',{
     arrows: false,
     pagination: true,
+    wheel: true,
+    speed: 2000,
     classes: {
         pagination: 'splide__pagination direction__pagination col-6 col-sm-5 col-md-5 col-lg-5 col-xl-5',
     },
@@ -63,3 +63,56 @@ mutationObserver.observe(direction, {
 });
 
 
+const isTouchDevice = 'ontouchstart' in document.documentElement;
+disableScroll();
+if (!isTouchDevice) smoothScroll();
+window.onresize = () => {
+    resizeBodyHeight();
+};
+window.onload = () => {
+    enableScroll();
+    resizeBodyHeight();
+};
+// Functions
+function disableScroll() {
+    document.body.style.overflow = 'hidden';
+}
+function enableScroll() {
+    document.body.style.overflow = '';
+}
+function smoothScroll() {
+    document.querySelector('#app').classList.add('SmoothScroll');
+    new SmoothScroll({
+        target: document.querySelector('#scroller-wrapper'),
+        scrollEase: 0.08,
+        maxOffset: 500,
+    });
+}
+function resizeBodyHeight() {
+    document.body.style.height = document.querySelector('#app').scrollHeight + 'px';
+}
+const backButton = document.querySelector('.back-button');
+const whoAreWe = document.querySelector('#who-are-we');
+const whatAreWeDoing = document.querySelector('#what-are-we-doing');
+const workProcess = document.querySelector('#work-process');
+const ourTeam = document.querySelector('#our-team');
+const ourClients = document.querySelector('#our-clients');
+backButton.addEventListener('click', () => {
+    window.scroll(0, 0);
+})
+
+whoAreWe.addEventListener('click', () => {
+    window.scroll(0, document.querySelector('#about-us').offsetTop);
+})
+whatAreWeDoing.addEventListener('click', () => {
+    window.scroll(0, (document.querySelector('#our-cases').offsetTop) - 20);
+})
+workProcess.addEventListener('click', () => {
+    window.scroll(0, (document.querySelector('#direction').offsetTop) - 20);
+})
+ourTeam.addEventListener('click', () => {
+    window.scroll(0, (document.querySelector('#team').offsetTop) - 20);
+})
+ourClients.addEventListener('click', () => {
+    window.scroll(0, (document.querySelector('.our-clients__title').offsetTop) - 100);
+})
