14dfabc803e2548e1fcde0e37aa5fab964cadfb9
Kerimov Farid <kerimov.f@platrum.ru>
2022-07-20 14:38:16 +0300

    dev

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,14 +8,14 @@ const backButton = document.querySelector('.back-button');
 
 setTimeout(function() {
     preHeaderText.classList.add('pre-header__content-text-active');
-}, 1);
+}, 2000);
 setTimeout(function() {
     preHeader.classList.add('pre-header-hide');
 
-}, 1);
+}, 4000);
 setTimeout(function() {
     preHeader.style.display = "none";
-}, 1);
+}, 5000);
 
 menuBurger.addEventListener('click',() =>{
     menu.classList.toggle('menu-active');
